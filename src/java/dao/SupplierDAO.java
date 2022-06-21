e93471650274f23092ec41df34feeff8e7a8393d
it.baonk <khacbao.1007@gmail.com>
2022-06-21 09:57:30 +0700

    Change DAO

---
+++ src/java/dao/SupplierDAO.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import Entity.Supplier;
+import java.util.List;
+
+/**
+ *
+ * @author KhacBao
+ */
+public interface SupplierDAO {
+    public int getTotalSupplier() throws Exception;
+    public List<Supplier> getAllSupplier() throws Exception;
+}
