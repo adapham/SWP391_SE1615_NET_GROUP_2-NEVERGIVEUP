08c295cd939434f1e7992183a74ad3c30b4998c5
tuyenneee <105269592+tuyenneee@users.noreply.github.com>
2022-07-10 16:06:38 +0700

    update project

--- src/java/Controller/BillManagerController.java
+++ src/java/Controller/BillManagerController.java
@@ -7,15 +7,19 @@ package Controller;
 
 import Entity.Order;
 import Entity.OrderDetails;
+import Entity.Product;
 import dao.impl.OrderDAOImpl;
 import dao.impl.OrderDetailsDAOImpl;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -41,21 +45,77 @@ public class BillManagerController extends HttpServlet {
             if (service == null) {
                 request.getRequestDispatcher("admin.jsp").forward(request, response);
             }
-            if (service.equals("bill")) {
-                OrderDAOImpl dao = new OrderDAOImpl();
-                List<Order> list = dao.listAllOrders();
-                request.setAttribute("list", list);
-                request.getRequestDispatcher("listBill.jsp").forward(request, response);
-            }
             if (service.equals("updateStatus")) {
                 OrderDAOImpl dao = new OrderDAOImpl();
                 String id = request.getParameter("odId");
+                String pageStr = request.getParameter("page");
+                int page = 1;
+                final int PAGE_SIZE = 5;
+                if (pageStr != null) {
+                    page = Integer.parseInt(pageStr);
+                }
+                int totalOrder = dao.getTotalOrder();
+                int totalPage = totalOrder / PAGE_SIZE;
+                if (totalOrder % PAGE_SIZE != 0) {
+                    totalPage += 1;
+                }
                 //Convert
                 int odID = Integer.parseInt(id);
                 int Status = Integer.parseInt(request.getParameter("status"));
                 int n = dao.updateStatus(Status, odID);
-                response.sendRedirect("billManager?do=bill");
+                request.setAttribute("page", page);
+                request.setAttribute("totalPage", totalPage);
+                response.sendRedirect("billManager?do=pageBill&page=" + page);
 
+            }
+            if (service.equals("pageBill")) {
+                OrderDAOImpl dao = new OrderDAOImpl();
+                HttpSession session = request.getSession();
+                String pageStr = request.getParameter("page");
+                int page = 1;
+                final int PAGE_SIZE = 5;
+                if (pageStr != null) {
+                    page = Integer.parseInt(pageStr);
+                }
+                List<Order> listOrder = dao.getOrderWithPaging(page, PAGE_SIZE);
+                int totalOrder = dao.getTotalOrder();
+                int totalPage = totalOrder / PAGE_SIZE;
+                if (totalOrder % PAGE_SIZE != 0) {
+                    totalPage += 1;
+                }
+                request.setAttribute("page", page);
+                request.setAttribute("totalPage", totalPage);
+                request.setAttribute("listOrder", listOrder);
+                request.getRequestDispatcher("listBill.jsp").forward(request, response);
+            }
+            if (service.equals("searchBill")) {
+                OrderDAOImpl dao = new OrderDAOImpl();
+                HttpSession session = request.getSession();
+                String keySearch = request.getParameter("keySearch");
+                if (keySearch.isEmpty()) {
+                    response.sendRedirect("billManager?do=pageBill");
+                    return;
+                }
+                String pageStr = request.getParameter("page");
+                int page = 1;
+                final int PAGE_SIZE = 5;
+                if (pageStr != null) {
+                    page = Integer.parseInt(pageStr);
+                }
+                List<Order> listOrder = dao.getSearchOrderPagingByAddress(keySearch, page, PAGE_SIZE);
+                int totalOrder = dao.getTotalOrderByAddress(keySearch);
+                int totalPage = totalOrder / PAGE_SIZE;
+                if (totalOrder % PAGE_SIZE != 0) {
+                    totalPage += 1;
+                }
+                String search = "1";
+                request.setAttribute("search", search);
+                request.setAttribute("keySearch", keySearch);
+                request.setAttribute("totalOrder", totalOrder);
+                request.setAttribute("page", page);
+                request.setAttribute("totalPage", totalPage);
+                request.setAttribute("listOrder", listOrder);
+                request.getRequestDispatcher("listBill.jsp").forward(request, response);
             }
             if (service.equals("details")) {
                 OrderDetailsDAOImpl dao = new OrderDetailsDAOImpl();
@@ -63,6 +123,16 @@ public class BillManagerController extends HttpServlet {
                 int oID = Integer.parseInt(odID);
                 List<OrderDetails> list = dao.getDetailsBill(oID);
                 OrderDetails info = dao.getInfoBill(oID);
+                double totalMoney = 0;
+                for (OrderDetails list1 : list) {
+                    totalMoney += list1.getPrice()* list1.getQuantity();
+                    totalMoney *= 100;
+                    double total = Math.ceil(totalMoney);
+                    total = (double) total / 100;
+                    totalMoney = total;
+
+                }
+                request.setAttribute("totalMoney", totalMoney);
                 request.setAttribute("info", info);
                 request.setAttribute("list", list);
                 request.setAttribute("orderID", oID);
@@ -78,6 +148,7 @@ public class BillManagerController extends HttpServlet {
                 request.getRequestDispatcher("billManager?do=details&odID=" + id).forward(request, response);
             }
         } catch (Exception ex) {
+            ex.printStackTrace();
             request.getRequestDispatcher("error500.jsp").forward(request, response);
         }
     }
